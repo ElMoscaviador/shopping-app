26152c772cfb093b468eecd61d5c46beaf4ae2f3
ElMoscaviador <aurelien.flieger@protonmail.com>
2023-03-14 13:24:20 +0100

    Add basename for GitHub Pages

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -43,7 +43,11 @@ const routes = [
 ];
 
 const Root = () => {
-  return <RouterProvider router={createBrowserRouter(routes)} />;
+  return (
+    <RouterProvider
+      router={(createBrowserRouter(routes), { basename: "eshop" })}
+    />
+  );
 };
 
 export default Root;
