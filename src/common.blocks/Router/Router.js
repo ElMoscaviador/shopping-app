f2fb8580365f43c73f801ece7dabb01d77f5dfee
ElMoscaviador <aurelien.flieger@protonmail.com>
2023-04-07 19:30:15 +0200

    Updayte hash basename

--- src/common.blocks/Router/Router.js
+++ src/common.blocks/Router/Router.js
@@ -46,7 +46,7 @@ const routes = [
   },
 ];
 
-const Router = () => <RouterProvider router={createHashRouter(routes, {basename: './shopping-app'})} />;
+const Router = () => <RouterProvider router={createHashRouter(routes, {basename: '/'})} />;
 
 export default Router;
 export { routes };
